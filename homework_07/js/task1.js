4ca15ba81502a1094f97828f39584de61f3ba0c8
Roman Pavliv <rpavliv37@gmail.com>
2018-02-08 23:30:26 +0200

    task 1 added

---
+++ homework_07/js/task1.js
@@ -0,0 +1,17 @@
+var floors = Number(prompt("enter natural number from 1 to 20", 1));
+var brick = '';
+if (isNaN(floors) || floors <= 0 || floors > 20) {
+    console.log('Incorrect!')
+} else {
+    for (var i = 1; i <= floors; i++) {
+        for (var k = 1; k <= (floors - i); k++) {
+            brick = brick.concat('   ');
+        }
+        for (var j = 2; j <= i; j++) {
+            brick = brick.concat('[~][~]');
+        }
+        brick = brick.concat('[~]\n');
+    }
+}
+
+console.log(brick);
