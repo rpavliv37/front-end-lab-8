ee50876ab9ffad49ae38f6d8268752a46b06ec11
Roman Pavliv <rpavliv37@gmail.com>
2018-02-13 21:33:29 +0200

    delete block "else"

--- homework_08/js/isPrime.js
+++ homework_08/js/isPrime.js
@@ -2,9 +2,8 @@ function isPrime(n) {
     for (let i = 2; i < n; i++) {
         if (n % i !== 0) {
             continue;
-        } else {
-            return false;
         }
+        return false;
     }
     return true;
 }
