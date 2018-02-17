08ab70383e06eb2d18639abb9bf80301642f384b
Roman Pavliv <rpavliv37@gmail.com>
2018-02-17 23:59:56 +0200

    added task 4

---
+++ homework_09/js/collectIds.js
@@ -0,0 +1,8 @@
+function collectIds (array) {
+	let collectedArray = getFilteredArray(array, function(element){
+		return element.rating > 3.0;
+	});
+	collectedArray = getTransformedArray(collectedArray, function(element){
+		return element.id;
+	})
+}
