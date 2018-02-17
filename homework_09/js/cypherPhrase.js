df95f8b426077e41958af6babd2bd5ea8e534d3c
Roman Pavliv <rpavliv37@gmail.com>
2018-02-18 00:00:41 +0200

    added task 5

---
+++ homework_09/js/cypherPhrase.js
@@ -0,0 +1,15 @@
+function cypherPhrase (object, string) {
+	string = string.split('');
+
+	string = getTransformedArray(string, function(element){
+		for(let key in object){
+			if (element === key) {
+				element = object[key]; 
+			}
+		}
+
+		return element;
+	})
+
+	return string.join('');
+}
