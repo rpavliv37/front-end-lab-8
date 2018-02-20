d1d19e46ba8de17e566ce37d5d0aafb94d6cdc8f
Roman Pavliv <rpavliv37@gmail.com>
2018-02-20 23:16:31 +0200

    added array

--- homework_09/js/collectIds.js
+++ homework_09/js/collectIds.js
@@ -1,8 +1,38 @@
-function collectIds (array) {
-	let collectedArray = getFilteredArray(array, function(element){
-		return element.rating > 3.0;
-	});
-	collectedArray = getTransformedArray(collectedArray, function(element){
-		return element.id;
-	})
+var movies = [{
+    "id": 70111470,
+    "title": "Die Hard",
+    "boxart": " http://bb-1/654356453",
+    "uri": " http://bb-1/654356453",
+    "rating": 2.7,
+    "bookmark": []
+}, {
+    "id": 654356453,
+    "title": "Good Mood",
+    "boxart": "http://bb-1",
+    "uri": " http://bb-1/654356453",
+    "rating": 3.5,
+    "bookmark": [{ time: 65876586 }]
+}, {
+    "id": 65432445,
+    "title": "The Chamber",
+    "boxart": "http://ch-1",
+    "uri": " http://ch-1/65432445",
+    "rating": 3.0,
+    "bookmark": []
+}, {
+    "id": 675465,
+    "title": "Fracture",
+    "boxart": "http://fr-1",
+    "uri": " http://fr-1/675465",
+    "rating": 4.0,
+    "bookmark": [{ time: 65876586 }]
+}];
+
+function collectIds(array) {
+    let collectedArray = getFilteredArray(array, function(element) {
+        return element.rating > 3.0;
+    });
+    collectedArray = getTransformedArray(collectedArray, function(element) {
+        return element.id;
+    })
 }
