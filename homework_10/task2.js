20f9185cdc9c3401c18447ee5bdfb4dd198bf83b
Roman Pavliv <rpavliv37@gmail.com>
2018-02-23 16:56:39 +0200

    added randomly block

--- homework_10/task2.js
+++ homework_10/task2.js
@@ -18,7 +18,7 @@ function fighter(info) {
         } else {
             defender.getStats().hp -= stats.attack;
             let madeAnyDmg = defenderOdlHp > defender.getStats().hp; // true if made somedmg
-            if (madeAnyDmg) {
+            if (madeAnyDmg && defender.getStats().hp <= 0) {
                 history.wins++;
                 defender.getCombatHistory().loses++;
             };
@@ -28,7 +28,7 @@ function fighter(info) {
     };
 
     block = () => {
-        return false;
+        return !!Math.floor(Math.random() * 2); // block randomly
     };
 
     getStats = () => stats;
@@ -46,18 +46,13 @@ function fighter(info) {
 
 
 
-var fighter1 = fighter({name: 'John', attack: 100, hp: 100});
-var fighter2 = fighter({name: 'Kayn', attack: 50, hp: 300});
-var fighter3 = fighter({name: 'Bill', attack: 40, hp: 100});
+let fighter1 = fighter({ name: 'John', attack: 100, hp: 100 });
+let fighter2 = fighter({ name: 'Kayn', attack: 50, hp: 300 });
+let fighter3 = fighter({ name: 'Bill', attack: 40, hp: 100 });
 
 fighter1.fight(fighter2); // true, fighter 1 make damage to fighter 2
 fighter1.fight(fighter3); // true, fighter 1 make damage to fighter 3
 
-
 showResult(fighter1);
-
-
-showResult(fighter2); 
-
-
+showResult(fighter2);
 showResult(fighter3);
